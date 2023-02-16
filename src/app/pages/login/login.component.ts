aa1601e0048aef01b63276995c5806cfc1867395
Gustavo <gustavo.magalhaes@mobilus.com.br>
2023-02-16 14:37:40 -0200

    tela de login criada

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { regexEmail, regexSenha } from 'src/app/utils/constantes';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private readonly router: Router
+  ){
+    this.loginForm = this.formBuilder.group({
+      email: ['',[Validators.required, Validators.pattern(regexEmail)]],
+      senha: ['', [Validators.required, Validators.pattern(regexSenha)]],
+    })
+  }
+ 
 
 }
